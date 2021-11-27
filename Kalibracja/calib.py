9a70ed8cfba853b8f551e8a28951045ecf2b3811
Jakub <koszowskimtb@gmail.com>
2021-11-27 14:51:33 +0100

    moving show img to function

--- Kalibracja/calib.py
+++ Kalibracja/calib.py
@@ -11,12 +11,13 @@ objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
-#obrazy z poprawnie wykrytą szachownicą dla prawej kamery - par wewn.
+# obrazy z poprawnie wykrytą szachownicą dla prawej kamery - par wewn.
 imagesRightCam = []
-#obrazy z poprawnie wykrytą szachownicą dla lewej kamery - par wewn.
+# obrazy z poprawnie wykrytą szachownicą dla lewej kamery - par wewn.
 imagesLeftCam = []
-#obrazy z poprawnie wykrytą szachownicą dla jednej z kamer to mają być pary zdjęć dla prawej i lewej - to do parametrow zewnetrznych
+# obrazy z poprawnie wykrytą szachownicą dla jednej z kamer to mają być pary zdjęć dla prawej i lewej - to do parametrow zewnetrznych
 imagesLeftRightCam = []
+
 
 def calib_cam():
     # get relative path
@@ -33,20 +34,22 @@ def calib_cam():
             print('Error cvtColor {}'.format(fname))
             continue
         # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(gray, (6, 8), None)
+        ret, corners = cv.findChessboardCorners(img, (6, 8), None)
         # If found, add object points, image points (after refining them)
-        if ret == True:
+        if ret:
             handle_add_to_list(fname)
             objpoints.append(objp)
-            corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             imgpoints.append(corners)
             # Draw and display the corners
-            cv.drawChessboardCorners(img, (6, 8), corners2, ret)
-            cv.imshow('img', img)
-            cv.waitKey(500)
+            show_img(img, gray, corners)
     cv.destroyAllWindows()
 
 
+def show_img(img, gray, corners):
+    corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+    cv.drawChessboardCorners(img, (6, 8), corners2)
+    cv.imshow('img', img)
+    cv.waitKey(500)
 
 
 def handle_add_to_list(filename):
@@ -54,6 +57,7 @@ def handle_add_to_list(filename):
         imagesLeftCam.append(filename)
     elif filename.find('right') >= 0:
         imagesRightCam.append(filename)
+
 
 def main():
     calib_cam()
