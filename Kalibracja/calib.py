6a8fbb28101f676b6033645652c9dcea6ccea335
Jakub <koszowskimtb@gmail.com>
2021-11-28 19:00:18 +0100

    single cam config implemented

--- Kalibracja/calib.py
+++ Kalibracja/calib.py
@@ -40,6 +40,7 @@ def provide_date_for_calib():
     images = glob.glob('*.png')
     for fname in images:
         print('filename: {}'.format(fname))
+        # img = cv2.imread('sample_image.png', cv2.IMREAD_COLOR) could be put in try cache
         img = cv.imread(fname)
         try:
             gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -47,14 +48,17 @@ def provide_date_for_calib():
             print('Error cvtColor {}'.format(fname))
             continue
         # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(img, CHESSBOARD_SIZE, None)
+        ret, corners = cv.findChessboardCorners(gray, CHESSBOARD_SIZE, None)
         # If found, add object points, image points (after refining them)
-        if ret:      
+        if ret:
             # for stereo calib use
             # corners = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             handle_add_to_list(fname, corners)
             # Draw and display the corners
             # show_img(img, gray, corners)
+    # I do not like the solution, but do not have idea for better one in this moment
+    global imgForCalib
+    imgForCalib = img
     cv.destroyAllWindows()
 
 
@@ -73,7 +77,7 @@ def handle_add_to_list(filename, corners):
         imgpointsLeft.append(corners)
     elif filename.find('right') >= 0:
         imagesRightCam.append(filename)
-        objpointsLeft.append(objp)
+        objpointsRight.append(objp)
         imgpointsRight.append(corners)
 
 
@@ -93,21 +97,56 @@ def get_number_index(filename):
     return filename.find('_') + 1
 
 
-def save_to_csv():
-    with open("output.csv", "w", encoding='UTF8', newline='') as f:
+def save_to_csv(listToSave, fileName):
+    with open("{}.csv".format(fileName), "w", encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
-        writer.writerows(imagesLeftRightCam)
+        writer.writerows(listToSave)
+
+
+def calib_single_cam(objpointsArg, imgpointsArg):
+    # Calibration
+    ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpointsArg, imgpointsArg, FRAME_SIZE, None, None)
+
+    height, width, channels = imgForCalib.shape
+    newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, dist, (width, height), 1, (width, height))
+
+    # Undistort
+    dst = cv.undistort(imgForCalib, cameraMatrix, dist, None, newCameraMatrix)
+
+    # Undistort with Remapping
+    mapx, mapy = cv.initUndistortRectifyMap(cameraMatrix, dist, None, newCameraMatrix, (width, height), 5)
+    dst = cv.remap(imgForCalib, mapx, mapy, cv.INTER_LINEAR)
+
+    # Not sure which matrix we should save
+    return cameraMatrix, dst
+
+def save_single_calib_to_xml(cameraMatrix, map, filename):
+    cv_file = cv.FileStorage('{}.xml'.format(filename), cv.FILE_STORAGE_WRITE)
+    cv_file.write('camera_matrix', cameraMatrix)
+    cv_file.write('map_x',map[0])
+    cv_file.write('map_y',map[1])
+
+    cv_file.release()
 
 
 def main():
     start = time.time()
     provide_date_for_calib()
+
+    # Left Cam
+    cameraMatrix, dst = calib_single_cam(objpointsLeft, imgpointsLeft)
+    save_single_calib_to_xml(cameraMatrix, dst, "leftCamConfig")
+
+    # Right Cam
+    cameraMatrix, dst = calib_single_cam(objpointsRight, imgpointsRight)
+    save_single_calib_to_xml(cameraMatrix, dst, "rightCamConfig")
+
     create_list_img_left_right()
     print("Run Time = {:.2f}".format(time.time() - start))
     # print(imagesLeftCam)
     # print(imagesRightCam)
-    print(imagesLeftRightCam)
-    save_to_csv()
+    # print(imagesLeftRightCam)
+    save_to_csv(imagesLeftRightCam, "imagesLeftRightCam")
 
 
 if __name__ == "__main__":
