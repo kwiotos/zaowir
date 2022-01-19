1677b23737a500b50365c5a6c7866b8fe8524250
Jakub <koszowskimtb@gmail.com>
2022-01-19 10:51:30 +0100

    reformating code

--- Kalibracja/rectify.py
+++ Kalibracja/rectify.py
@@ -7,7 +7,8 @@ def rectify(cameraMatrix, dist, rect, projMatrix, imgRectify):
     # Undistort with Remapping
     mapx, mapy = cv.initUndistortRectifyMap(
         cameraMatrix, dist, rect, projMatrix, imgRectify.shape[::-1], cv.CV_16SC2)
-    dst = cv.remap(imgRectify, mapx, mapy, cv.INTER_LANCZOS4, cv.BORDER_CONSTANT, 0)
+    dst = cv.remap(imgRectify, mapx, mapy,
+                   cv.INTER_LANCZOS4, cv.BORDER_CONSTANT, 0)
     return dst
 
 
@@ -16,19 +17,23 @@ def stereoRectify(leftPic, rightPic):
     data = loadFromJson("stereo_calib_config.json")
 
     # Stereo Rectification
-    rectifyScale= 1
-    rectL, rectR, projMatrixL, projMatrixR, Q, roi_L, roi_R = cv.stereoRectify(data['newCameraMatrixL'], data['distL'], data['newCameraMatrixR'], data['distR'], imgForCalib.shape[::-1], data['rot'], data['trans'], rectifyScale,(0,0))
+    rectifyScale = 1
+    rectL, rectR, projMatrixL, projMatrixR, Q, roi_L, roi_R = cv.stereoRectify(
+        data['newCameraMatrixL'], data['distL'], data['newCameraMatrixR'], data['distR'], leftPic.shape[::-1], data['rot'], data['trans'], rectifyScale, (0, 0))
 
-    leftCamPhoto = rectify(data['newCameraMatrixL'], data['distL'], rectL, projMatrixL, imgRectify=leftPic)
-    leftCamPhoto = rectify(data['newCameraMatrixR'], data['distR'], rectR, projMatrixR, imgRectify=rightPic)
+    leftCamPhoto = rectify(
+        data['newCameraMatrixL'], data['distL'], rectL, projMatrixL, imgRectify=leftPic)
+    leftCamPhoto = rectify(
+        data['newCameraMatrixR'], data['distR'], rectR, projMatrixR, imgRectify=rightPic)
 
     # missing show pics
+
 
 def main():
     start = time.time()
 
-    stereoRectify(None, None) # missing pics
-    
+    stereoRectify(None, None)  # missing pics
+
     print("Run Time = {:.2f}".format(time.time() - start))
 
 
